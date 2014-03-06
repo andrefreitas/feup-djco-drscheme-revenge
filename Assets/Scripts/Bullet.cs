5e570930a6755207272e01210fa64bf0afda5ba6
Ana Gomes <acgg.gomes@gmail.com>
2014-03-06 16:17:32 +0000

    Ricochete semi-funcional. A avaliar se se pode melhorar o efeito ou não.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,16 +3,29 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour {
 
+	public LayerMask collisionMask;
+	private float speed = 15;
 	// Use this for initialization
 	void Start () {
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Ray ray = new Ray(transform.position, transform.forward);
+		RaycastHit hit;
 		
+		if (Physics.Raycast(ray, out hit, Time.deltaTime * speed + .1f,collisionMask)) {
+			Vector3 reflectDir = Vector3.Reflect(ray.direction, hit.normal);
+			float rot = 90 - Mathf.Atan2(reflectDir.y, reflectDir.x) * Mathf.Rad2Deg;
+			transform.eulerAngles = new Vector3(0,rot,0);
+		}
 	}
 
 	void OnCollisionEnter(Collision col) {
-		Destroy(gameObject);
+		if (col.gameObject.name == "LimitCollider") {
+			Debug.Log("LimitCollider");
+		}
+		else Destroy(gameObject);
 	}
 }
