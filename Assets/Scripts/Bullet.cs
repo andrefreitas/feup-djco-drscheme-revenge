bf6c77b5bb788f65de873ef24c3933d801b1f392
Joaquim Barros <jruirbarros@gmail.com>
2014-03-10 11:49:14 +0000

    Bullets out of space destroyed

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -12,14 +12,20 @@ public class Bullet : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Ray ray = new Ray(transform.position, transform.forward);
-		RaycastHit hit;
+
+		if (transform.position.y >= 10) {
+						Destroy (gameObject);
+				} else {
+						Ray ray = new Ray (transform.position, transform.forward);
+						RaycastHit hit;
 		
-		if (Physics.Raycast(ray, out hit, Time.deltaTime * speed + .1f,collisionMask)) {
-			Vector3 reflectDir = Vector3.Reflect(ray.direction, hit.normal);
-			float rot = 90 - Mathf.Atan2(reflectDir.y, reflectDir.x) * Mathf.Rad2Deg;
-			transform.eulerAngles = new Vector3(0,rot,0);
-		}
+						if (Physics.Raycast (ray, out hit, Time.deltaTime * speed + .1f, collisionMask)) {
+								Vector3 reflectDir = Vector3.Reflect (ray.direction, hit.normal);
+								float rot = 90 - Mathf.Atan2 (reflectDir.y, reflectDir.x) * Mathf.Rad2Deg;
+								transform.eulerAngles = new Vector3 (0, rot, 0);
+						}
+
+				}
 	}
 
 	void OnCollisionEnter(Collision col) {
