743e247b397a136aa3ea7d8547a3bbcaa7d9bc4a
Joaquim Barros <jruirbarros@gmail.com>
2014-03-11 18:24:58 +0000

    Alterações na física da bala. Dado que depois se vai substituir o objeto, dá jeito manter a rotação.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,28 +3,28 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour {
 
-	public LayerMask collisionMask;
-
 	public float angle = 0;
+
 	// Use this for initialization
 	void Start () {
-
+		rigidbody.freezeRotation = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 		if (transform.position.y >= 10) {
-						Destroy (gameObject);
-				} else {
+				Destroy (gameObject);
+		} else {
 
-				}
+		}
 	}
 
 	void OnCollisionEnter(Collision col) {
 		if (col.gameObject.name == "LeftLimit" || col.gameObject.name == "RightLimit") {
 			Debug.Log("Wall collision...");
 		}
+
 		else Destroy(gameObject);
 	}
 }
