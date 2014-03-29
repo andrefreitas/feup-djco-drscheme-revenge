ebce029baa5ae8f522e4d97cbccf2c529bf6a6e4
André Freitas <p.andrefreitas@gmail.com>
2014-03-29 16:01:06 +0000

    Adicionados drs esquemas a mexerem-se sozinhos

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,27 +4,55 @@ using System.Collections;
 public class Enemy : MonoBehaviour {
 
 	public float shootProbability = 0.3f;
-	public float movementProbability = 0.3f;
+	public float movementProbability = 0.1f;
+	public float bounceProbability = 0.2f;
+	public float signalProbability = 0.2f;
+
+	public float speed = 5f;
+	public float force = 4f;
+	public int signal;
 
 	public GameObject player;
 	public GameObject ball;
 
 	void Start () {
 		Debug.Log ("Enemy!!");
+		signal = 1;
 	
 	}
 	
 
 	void Update () {
 
+		float randomNumber = Random.Range (0f, 1f);
+
+		if (randomNumber < movementProbability) {
+			Move ();
+		}
+
+		randomNumber = Random.Range (0f, 1f);
+		if (randomNumber < shootProbability) {
+			Shoot ();
+		}
 	
 	}
 
-	void ShootPlayer(){
+	void Move() {
+		float randomNumber = Random.Range (0f, 1f);
+		if (randomNumber < signalProbability ) {
+			signal *= -1;
+		}
+		rigidbody2D.AddForce(new Vector2(1, 0) * force * signal);
 
 	}
 
-	void Bounce() {
-
+	void Shoot() {
+		float randomNumber = Random.Range (0f, 1f);
+		if (randomNumber < signalProbability ) {
+			signal *= -1;
+		}
+		rigidbody2D.AddForce(new Vector2(1, 0) * force * signal);
+		
 	}
+
 }
