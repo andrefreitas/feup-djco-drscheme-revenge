53a58af65f938292bed2351e3f84878c8590ab48
André Freitas <p.andrefreitas@gmail.com>
2014-03-29 15:09:08 +0000

    Adicionada base do inimigo com múltiplas sprites.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float shootProbability = 0.3f;
+	public float movementProbability = 0.3f;
+
+	public GameObject player;
+	public GameObject ball;
+
+	void Start () {
+		Debug.Log ("Enemy!!");
+	
+	}
+	
+
+	void Update () {
+
+	
+	}
+
+	void ShootPlayer(){
+
+	}
+
+	void Bounce() {
+
+	}
+}
